e2a4f5f71495544d6cb085604d95ab13f2e2abe0
Nathan Campos <eeepc904@gmail.com>
2012-10-27 18:17:58 -0200

    A bit of styling

--- index.php
+++ index.php
@@ -9,10 +9,27 @@
 <html>
     <head>
         <title><?php echo $blog_title; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
+        <!-- Libraries -->
+        <link href="http://necolas.github.com/normalize.css/2.0.1/normalize.css" rel="stylesheet" type="text/css" />
+        <script src="http://code.jquery.com/jquery-1.8.2.min.js" type="text/javascript"></script>
+        
+        <!-- Theme -->
         <?php Style::css_file_includes($blog_theme_location, $blog_theme_name); ?>
     </head>
     <body>
-        
+        <header>
+            <div class="container">
+                <div class="pane">
+                    <h1>Blog Title</h1>
+                    <h2>Blog Subtitle goes here</h2>
+                </div>
+                
+                <div class="pane">
+                    <p>Some awesome text here because this is fucking awesome.</p>
+                </div>
+            </div>
+        </header>
     </body>
 </html>
