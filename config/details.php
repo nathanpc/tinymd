0573b91cb31aa2114c9eb73083d63da3b5325b05
Nathan Campos <eeepc904@gmail.com>
2012-10-27 12:31:25 -0200

    Configurations...

---
+++ config/details.php
@@ -0,0 +1,11 @@
+<?php
+
+// Blog Header.
+$blog_title = "Test Blog";
+$blog_subtitle = "Having some fun with tinymd";
+
+// Blog Theme.
+$blog_theme_location = "styles";
+$blog_theme_name = "default";
+
+?>
